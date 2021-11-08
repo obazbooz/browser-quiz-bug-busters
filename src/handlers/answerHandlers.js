00bf70527b1c8ce28c513f2723aeb19c7e882ceb
Osama Bazbooz <obazbooz@gmail.com>
2021-11-08 12:28:23 +0100

    user can know which question they got correct or incorrect! task has been donegit add .!

--- src/handlers/answerHandlers.js
+++ src/handlers/answerHandlers.js
@@ -1,10 +1,21 @@
 export const createSelectAnswerHandler = (question) => {
 
     return (event) => {
+      // user can select an answer for each question. 
         for (const key in question.answers) {
           if (question.answers[key] === event.target.textContent){
             question.selected = key;
           }
         }
+        // The user can know which questions are correct and incorrect.
+        if (question.selected === question.correct) {
+         event.target.classList.add('correctChoice');
+        }
+        else {
+          event.target.classList.add('incorrectChoice');
+        }
+
     }
 }
+
+
