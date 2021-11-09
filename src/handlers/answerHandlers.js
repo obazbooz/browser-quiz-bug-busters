bd2bcd59748320e5456c6e1a38b95f8878dc333d
Caglar Kaya <caglar.kaya.europe@gmail.com>
2021-11-09 11:30:15 +0100

    user can only choose once and bug fixed

--- src/handlers/answerHandlers.js
+++ src/handlers/answerHandlers.js
@@ -4,23 +4,26 @@ import { quizData } from "../data.js";
 export const createSelectAnswerHandler = (question) => {
 
   return (event) => {
-    const answerLi = document.querySelectorAll('li');
-    // user can select an answer for each question. 
-    for (const key in question.answers) {
-      if (key === event.target.getAttribute('data-answer-key')) {
-        question.selected = key;
+    // user can only choose once
+    if (question.selected === null) {
+      const answerLi = document.querySelectorAll('li');
+      // user can select an answer for each question. 
+      for (const key in question.answers) {
+        if (key === event.target.getAttribute('data-answer-key')) {
+          question.selected = key;
+        }
       }
-    }
-    // The user can know which questions are correct and incorrect.
-    if (question.selected !== question.correct) {
-      event.target.classList.add('incorrectChoice');
-    }
-    for (const element of answerLi) {
-      if (element.getAttribute('data-answer-key') === question.correct) {
-        element.classList.add('correctChoice');
+      // The user can know which questions are correct and incorrect.
+      if (question.selected !== question.correct) {
+        event.target.classList.add('incorrectChoice');
       }
+      for (const element of answerLi) {
+        if (element.getAttribute('data-answer-key') === question.correct) {
+          element.classList.add('correctChoice');
+        }
+      }
+      showQuizResult(quizData.questions, quizData.currentQuestionIndex + 1);
     }
-    showQuizResult(quizData.questions, quizData.currentQuestionIndex + 1);
   }
 }
 
