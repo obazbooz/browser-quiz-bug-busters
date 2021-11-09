fb07bab0f8a3170cfa4e766235541374a82f6466
Caglar Kaya <caglar.kaya.europe@gmail.com>
2021-11-09 11:01:00 +0100

    calculated score and display after finishing quiz

--- src/handlers/answerHandlers.js
+++ src/handlers/answerHandlers.js
@@ -1,25 +1,28 @@
-import { createQuestionElement } from "../views/questionViews.js";
+import { showQuizResult } from "./resultHandlers.js";
+import { quizData } from "../data.js";
 
 export const createSelectAnswerHandler = (question) => {
 
-    return (event) => {
-      const answerLi =  document.querySelectorAll('li');
-      // user can select an answer for each question. 
-        for (const key in question.answers) {
-          if (key === event.target.getAttribute('data-answer-key')) {
-            question.selected = key;
-          }
-        }
-        // The user can know which questions are correct and incorrect.
-        if (question.selected !== question.correct) {
-          event.target.classList.add('incorrectChoice');
-        }
-        for (const element of answerLi) {
-          if (element.getAttribute('data-answer-key') === question.correct) {
-            element.classList.add('correctChoice');
-          }
-        }
+  return (event) => {
+    const answerLi = document.querySelectorAll('li');
+    // user can select an answer for each question. 
+    for (const key in question.answers) {
+      if (key === event.target.getAttribute('data-answer-key')) {
+        question.selected = key;
       }
     }
+    // The user can know which questions are correct and incorrect.
+    if (question.selected !== question.correct) {
+      event.target.classList.add('incorrectChoice');
+    }
+    for (const element of answerLi) {
+      if (element.getAttribute('data-answer-key') === question.correct) {
+        element.classList.add('correctChoice');
+      }
+    }
+    showQuizResult(quizData.questions, quizData.currentQuestionIndex + 1);
+  }
+}
+
 
 
